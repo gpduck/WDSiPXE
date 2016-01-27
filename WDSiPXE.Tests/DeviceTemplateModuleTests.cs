e99cf49dd71971b7bc832813c27323f23f0a1d02
Chris Duck <gpduck@whatsupduck.net>
2016-01-27 01:13:02 -0600

    Fix DeviceTemplateModule tests

--- WDSiPXE.Tests/DeviceTemplateModuleTests.cs
+++ WDSiPXE.Tests/DeviceTemplateModuleTests.cs
@@ -13,7 +13,8 @@ namespace WDSiPXE.Tests
         [TestMethod]
         public void GetDevicebyId()
         {
-            Browser browser = new Browser(new Bootstrapper(Directory.GetCurrentDirectory()));
+            DeviceTemplateModule module = new DeviceTemplateModule(new FakeDeviceRepository());
+            Browser browser = new Browser(with => with.Module(module));
             BrowserResponse response = browser.Get("/ipxe/00-00-00-00-00-01", c => {
                 //c.Header("Accept", "text/plain");
                 c.HostName("localhost");
---
+++ WDSiPXE.Tests/FakeDeviceRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WDSiPXE;
+
+namespace WDSiPXE.Tests
+{
+    class FakeDeviceRepository : IDeviceRepository
+    {
+        public Device GetDeviceById(string id)
+        {
+            Device d = new Device();
+            d.Add("netbootGuid", id);
+            return d;
+        }
+    }
+}
