f6516a33a078cf890daeffbf7c46f1574df3e501
Chris Duck <gpduck@whatsupduck.net>
2016-01-30 15:18:55 -0600

    Update tests

--- WDSiPXE.Tests/DeviceTemplateModuleTests.cs
+++ WDSiPXE.Tests/DeviceTemplateModuleTests.cs
@@ -13,14 +13,20 @@ namespace WDSiPXE.Tests
         [TestMethod]
         public void GetDevicebyId()
         {
-            DeviceTemplateModule module = new DeviceTemplateModule(new FakeDeviceRepository());
+            FakeDeviceRepository fdr = new FakeDeviceRepository();
+            Device d = new Device();
+            String netbootGuid = "00-00-00-00-00-01";
+            d.Add("netbootGuid", netbootGuid);
+            fdr.Devices.Add(netbootGuid, d);
+            DeviceTemplateModule module = new DeviceTemplateModule(fdr);
             Browser browser = new Browser(with => with.Module(module));
-            BrowserResponse response = browser.Get("/ipxe/00-00-00-00-00-01", c => {
+            BrowserResponse response = browser.Get("/DeviceTemplateModuleTests/" + netbootGuid, c => {
                 //c.Header("Accept", "text/plain");
                 c.HostName("localhost");
-                c.Query("Template", "esx55");
+                c.Query("Template", "netbootguid");
             });
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.AreEqual(response.Body.AsString(), netbootGuid);
         }
     }
 }
--- WDSiPXE.Tests/FakeDeviceRepository.cs
+++ WDSiPXE.Tests/FakeDeviceRepository.cs
@@ -9,11 +9,19 @@ namespace WDSiPXE.Tests
 {
     class FakeDeviceRepository : IDeviceRepository
     {
+        public Dictionary<String, Device>Devices = new Dictionary<String, Device>();
+
+
         public Device GetDeviceById(string id)
         {
-            Device d = new Device();
-            d.Add("netbootGuid", id);
-            return d;
+            if (Devices.ContainsKey(id))
+            {
+                return Devices[id];
+            }
+            else
+            {
+                throw new DeviceNotFoundException("Device with id " + id + " not found.");
+            }
         }
     }
 }
